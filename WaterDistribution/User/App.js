fc6f04e503d08f8482231a19c26b76e26fb73b2b
Mehara Sahabandu <sahabandumehara@gmail.com>
2024-04-22 11:48:46 +0530

    Home Screen completed. Single page started

---
+++ WaterDistribution/User/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import HomeScreen from "./src/screens/HomeScreen";
+import RestaurantDetailsPage from "./src/screens/RestaurantDetailsScreen";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      {/* Resturant Items */}
+      {/* <HomeScreen /> */}
+      <RestaurantDetailsPage />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    // padding: 10,
+    // paddingVertical:30
+  },
+});
