e03ea8fc3d89d3523bc35a3ca2513f9433dc57aa
Mehara Sahabandu <sahabandumehara@gmail.com>
2024-04-27 08:49:36 +0530

    auth page done

--- WaterDistribution/User/App.js
+++ WaterDistribution/User/App.js
@@ -2,8 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import RootNavigator from "./src/navigation";
 import { NavigationContainer } from "@react-navigation/native";
+import { withAuthenticator } from "aws-amplify-react-native";
+import { Amplify } from "aws-amplify";
+import config from "./src/aws-exports";
 
-export default function App() {
+Amplify.configure(config);
+
+function App() {
   return (
     <NavigationContainer>
       <RootNavigator />
@@ -11,3 +16,4 @@ export default function App() {
     </NavigationContainer>
   );
 }
+export default withAuthenticator(App);
